634e780d9c05993752c503aff866ee70bb446ef8
SatoSeiichi <s.sato@1-10.com>
2017-02-16 20:11:09 +0900

    ファーストコミット

---
+++ Assets/Scrips/ZigReciever.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+public class ZigReciever : MonoBehaviour {
+
+	public delegate void OnReciveDataCallback(string data);
+
+	UdpClient udp;
+	public int portNo=50000;
+	Thread thread;
+
+	OnReciveDataCallback OnReceiveData;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDestroy(){
+		if(udp!=null){
+			udp.Close ();
+			thread.Abort ();
+		}
+	}
+
+	void OnThreading(){
+		while (true) {
+			IPEndPoint remoteEP = null;
+			byte[] data = udp.Receive (ref remoteEP);
+			string text = Encoding.ASCII.GetString (data);
+			if (OnReceiveData != null) {
+				OnReceiveData (text);
+			}
+		}
+	}
+
+	public void SetUp(OnReciveDataCallback callback){
+		OnReceiveData = callback;
+		udp = new UdpClient (portNo);
+		thread = new Thread (new ThreadStart(OnThreading));
+		thread.Start ();
+	}
+
+}
---
+++ Assets/Scrips/ZigScriptDraw.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using DigitalRuby.AnimatedLineRenderer;
+
+public class ZigScriptDraw : MonoBehaviour {
+	public AnimatedLineRenderer AnimatedLine;
+	float x = 0;
+	float y = 0;
+
+	bool tachiFlag = false;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<ZigReciever> ().SetUp (OnReciveZigData);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (AnimatedLine == null)
+		{
+			return;
+		}
+		else if (Input.GetMouseButton(0))
+		{
+			Vector3 pos = Input.mousePosition;
+			print (pos);
+			pos = Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, AnimatedLine.transform.position.z));
+
+			AnimatedLine.Enqueue(pos);
+		}
+
+		if (AnimatedLine == null)
+		{
+			return;
+		}
+		else if (tachiFlag) {
+			float x = 0;
+			x = (Screen.width / 2) * (this.x + 1);
+
+			float y = 0;
+			y = (Screen.height / 2) * (this.y - 1) * -1;
+//			if (x >= 0) {
+//				
+//			} else if (x < 0) {
+//				x = 
+//			}
+
+			//Vector3 pos = Input.mousePosition;
+
+			print (this.y);
+			Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(x, y, AnimatedLine.transform.position.z));
+
+			AnimatedLine.Enqueue(pos);
+		}
+
+
+	}
+
+	void OnReciveZigData(string data){
+		JSONNode zigData = JSONNode.Parse (data);
+
+		//print (data);
+
+
+
+		JSONArray array = zigData["sensordata"]["touch"].AsArray;
+
+		for (int i = 0; i < array.Count; i++) {
+
+			x = array [i] ["x"].AsFloat;
+			y = array [i] ["y"].AsFloat;
+		}
+
+		//print (array.Count);
+		if (array.Count > 0) {
+			tachiFlag = true;
+		} else {
+			tachiFlag = false;
+		}
+		//print ("x=" + x);
+		//print ("y=" + y);
+
+
+	}
+}
