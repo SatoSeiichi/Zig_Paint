af8675a7e168f4efa0652609e7fc067692ca0b07
SatoSeiichi <s.sato@1-10.com>
2017-02-16 20:27:02 +0900

    ソース整理

--- Assets/Scrips/ZigScriptDraw.cs
+++ Assets/Scrips/ZigScriptDraw.cs
@@ -23,34 +23,12 @@ public class ZigScriptDraw : MonoBehaviour {
 		{
 			return;
 		}
-		else if (Input.GetMouseButton(0))
-		{
-			Vector3 pos = Input.mousePosition;
-			print (pos);
-			pos = Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, AnimatedLine.transform.position.z));
-
-			AnimatedLine.Enqueue(pos);
-		}
-
-		if (AnimatedLine == null)
-		{
-			return;
-		}
 		else if (tachiFlag) {
 			float x = 0;
 			x = (Screen.width / 2) * (this.x + 1);
 
 			float y = 0;
 			y = (Screen.height / 2) * (this.y - 1) * -1;
-//			if (x >= 0) {
-//				
-//			} else if (x < 0) {
-//				x = 
-//			}
-
-			//Vector3 pos = Input.mousePosition;
-
-			print (this.y);
 			Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(x, y, AnimatedLine.transform.position.z));
 
 			AnimatedLine.Enqueue(pos);
@@ -62,10 +40,6 @@ public class ZigScriptDraw : MonoBehaviour {
 	void OnReciveZigData(string data){
 		JSONNode zigData = JSONNode.Parse (data);
 
-		//print (data);
-
-
-
 		JSONArray array = zigData["sensordata"]["touch"].AsArray;
 
 		for (int i = 0; i < array.Count; i++) {
@@ -74,15 +48,11 @@ public class ZigScriptDraw : MonoBehaviour {
 			y = array [i] ["y"].AsFloat;
 		}
 
-		//print (array.Count);
 		if (array.Count > 0) {
 			tachiFlag = true;
 		} else {
 			tachiFlag = false;
 		}
-		//print ("x=" + x);
-		//print ("y=" + y);
-
 
 	}
 }
